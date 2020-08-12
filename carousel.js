0089cabbceb5dd9938e12f92b5882ab28c068e11
Lucy Toman <lucy@toman.me.uk>
2020-08-12 17:26:38 +0200

    adds logic to only run when screen is visible

--- carousel.js
+++ carousel.js
@@ -6,107 +6,118 @@
     let i = 0;
     let timer;
     let isTransitioning = false;
+    let screenVis = true;
 
-    timer = setTimeout(moveSlide, 7000);
+    if (screenVis) {
+        timer = setTimeout(moveSlide, 7000);
 
-    function moveSlide(index) {
-        isTransitioning = true;
-        if (typeof index == "number") {
-            if (i === index) {
-                return;
-            } else {
+        function moveSlide(index) {
+            isTransitioning = true;
+            if (typeof index == "number") {
+                if (i === index) {
+                    return;
+                } else {
+                    slide[i].classList.remove("onscreen");
+                    dots[i].classList.remove("fill-in");
+                    slide[i].classList.add("offscreen-left");
+
+                    i = index;
+
+                    dots[i].classList.add("fill-in");
+                    slide[i].classList.add("onscreen");
+                }
+            } else if (i < last) {
                 slide[i].classList.remove("onscreen");
                 dots[i].classList.remove("fill-in");
                 slide[i].classList.add("offscreen-left");
 
-                i = index;
+                i++;
+
+                dots[i].classList.add("fill-in");
+                slide[i].classList.add("onscreen");
+            } else if (i === last) {
+                slide[i].classList.remove("onscreen");
+                dots[i].classList.remove("fill-in");
+                slide[i].classList.add("offscreen-left");
+
+                i = 0;
 
                 dots[i].classList.add("fill-in");
                 slide[i].classList.add("onscreen");
             }
-        } else if (i < last) {
-            slide[i].classList.remove("onscreen");
-            dots[i].classList.remove("fill-in");
-            slide[i].classList.add("offscreen-left");
 
-            i++;
-
-            dots[i].classList.add("fill-in");
-            slide[i].classList.add("onscreen");
-        } else if (i === last) {
-            slide[i].classList.remove("onscreen");
-            dots[i].classList.remove("fill-in");
-            slide[i].classList.add("offscreen-left");
-
-            i = 0;
-
-            dots[i].classList.add("fill-in");
-            slide[i].classList.add("onscreen");
+            timer = setTimeout(moveSlide, 7000);
         }
 
-        timer = setTimeout(moveSlide, 7000);
-    }
+        for (let x = 0; x < dots.length; x++) {
+            dots[x].addEventListener("click", clickHandler(x));
+        }
 
-    for (let x = 0; x < dots.length; x++) {
-        dots[x].addEventListener("click", clickHandler(x));
-    }
-
-    function clickHandler(dotIndex) {
-        return function() {
-            if (isTransitioning) {
-                return;
-            } else {
-                for (let d = 0; d < dots.length; d++) {
-                    dots[d].classList.remove("fill-in");
+        function clickHandler(dotIndex) {
+            return function() {
+                if (isTransitioning) {
+                    return;
+                } else {
+                    for (let d = 0; d < dots.length; d++) {
+                        dots[d].classList.remove("fill-in");
+                    }
+                    dots[dotIndex].classList.add("fill-in");
+                    clearTimeout(timer);
+                    moveSlide(dotIndex);
                 }
-                dots[dotIndex].classList.add("fill-in");
+            };
+        }
+
+        document
+            .getElementById("right")
+            .addEventListener("click", function rightClick() {
+                if (isTransitioning) {
+                    return;
+                } else {
+                    for (let d = 0; d < dots.length; d++) {
+                        dots[d].classList.remove("fill-in");
+                    }
+                }
+
+                dots[i].classList.add("fill-in");
                 clearTimeout(timer);
-                moveSlide(dotIndex);
+                moveSlide();
+            });
+
+        document
+            .getElementById("left")
+            .addEventListener("click", function leftClick() {
+                if (isTransitioning) {
+                    return;
+                } else {
+                    for (let d = 0; d < dots.length; d++) {
+                        dots[d].classList.remove("fill-in");
+                    }
+                }
+                if (i === 0) {
+                    index = dots.length - 1;
+                } else {
+                    index = i - 1;
+                }
+
+                dots[i].classList.add("fill-in");
+                clearTimeout(timer);
+                moveSlide(index);
+            });
+
+        document.addEventListener("transitionend", function(event) {
+            if (event.target.classList.contains("offscreen-left")) {
+                event.target.classList.remove("offscreen-left");
+                isTransitioning = false;
             }
-        };
+        });
     }
 
-    document
-        .getElementById("right")
-        .addEventListener("click", function rightClick() {
-            if (isTransitioning) {
-                return;
-            } else {
-                for (let d = 0; d < dots.length; d++) {
-                    dots[d].classList.remove("fill-in");
-                }
-            }
-
-            dots[i].classList.add("fill-in");
-            clearTimeout(timer);
-            moveSlide();
-        });
-
-    document
-        .getElementById("left")
-        .addEventListener("click", function leftClick() {
-            if (isTransitioning) {
-                return;
-            } else {
-                for (let d = 0; d < dots.length; d++) {
-                    dots[d].classList.remove("fill-in");
-                }
-            }
-            if (i === 0) {
-                index = dots.length - 1;
-            } else {
-                index = i - 1;
-            }
-
-            dots[i].classList.add("fill-in");
-            clearTimeout(timer);
-            moveSlide(index);
-        });
-
-    document.addEventListener("transitionend", function(event) {
-        if (event.target.classList.contains("offscreen-left")) {
-            event.target.classList.remove("offscreen-left");
-            isTransitioning = false;
+    document.addEventListener("visibilitychange", function() {
+        if (document.hidden === true) {
+            screenVis = false;
+        } else {
+            screenVis = true;
         }
     });
 })();
